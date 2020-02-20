715ead8d59e83e5819d67eac512d435f67af0c99
Lee Edwards <lee@root.vc>
2020-02-19 16:21:43 -0800

    Fix subscribe footer on mobile

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -74,7 +74,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
                             <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link>
+                                Subscribe to the lee.af Newsletter
                             </div>
                             <div className="site-foot-nav-right">
                                 <a name="subscribe" className="hidden"></a>
